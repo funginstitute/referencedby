dda7c8f684e9341f13a933ec2e0bd71b60fe92ee
Dave Doolin <david.doolin@gmail.com>
2012-12-07 21:54:18 -0800

    works with arbitrary patents having citing refs. Does not handle null result case.

--- referencedby.rb
+++ referencedby.rb
@@ -4,7 +4,8 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-patent = "5136185"
+#patent = "5136185"
+patent = "5539337"
 # URL for search on patent number defined above
 #patentquery = "http://patft.uspto.gov/netacgi/nph-Parser?Sect1=PTO2&Sect2=HITOFF&p=1&u=%2Fnetahtml%2FPTO%2Fsearch-bool.html&r=1&f=G&l=50&co1=AND&d=PTXT&s1=#{patent}.PN.&OS=PN/#{patent}&RS=PN/#{patent}"
 refquery = "http://patft.uspto.gov/netacgi/nph-Parser?Sect1=PTO2&Sect2=HITOFF&p=1&u=%2Fnetahtml%2Fsearch-adv.htm&r=0&f=S&l=50&d=PALL&Query=ref/#{patent}"
