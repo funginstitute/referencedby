000d98aef553cf5631e48ab9d3c5cb482aef2d52
Dave Doolin <david.doolin@gmail.com>
2012-12-07 22:26:35 -0800

    class wrapped, rspecced

--- referencedby.rb
+++ referencedby.rb
@@ -3,18 +3,32 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+require 'rspec'
+
+
+class ReffedBy
+
+  def initialize(patent)
+    # URL for search on patent number defined above
+    #patentquery = "http://patft.uspto.gov/netacgi/nph-Parser?Sect1=PTO2&Sect2=HITOFF&p=1&u=%2Fnetahtml%2FPTO%2Fsearch-bool.html&r=1&f=G&l=50&co1=AND&d=PTXT&s1=#{patent}.PN.&OS=PN/#{patent}&RS=PN/#{patent}"
+    refquery = "http://patft.uspto.gov/netacgi/nph-Parser?Sect1=PTO2&Sect2=HITOFF&p=1&u=%2Fnetahtml%2Fsearch-adv.htm&r=0&f=S&l=50&d=PALL&Query=ref/#{patent}"
+
+    patentpage = Nokogiri::HTML(open(refquery))
+
+    doc = patentpage.xpath("//body/table")
+    rows = doc.xpath("//table//tr[1 <= position() and position() < 200]/td[2]/a/text()")
+    #puts rows
+    rows.each do |r|
+      puts "0" + r.to_s.gsub(/,/,'')
+    end
+  end
+
+end
 
 #patent = "5136185"
 patent = "5539337"
-# URL for search on patent number defined above
-#patentquery = "http://patft.uspto.gov/netacgi/nph-Parser?Sect1=PTO2&Sect2=HITOFF&p=1&u=%2Fnetahtml%2FPTO%2Fsearch-bool.html&r=1&f=G&l=50&co1=AND&d=PTXT&s1=#{patent}.PN.&OS=PN/#{patent}&RS=PN/#{patent}"
-refquery = "http://patft.uspto.gov/netacgi/nph-Parser?Sect1=PTO2&Sect2=HITOFF&p=1&u=%2Fnetahtml%2Fsearch-adv.htm&r=0&f=S&l=50&d=PALL&Query=ref/#{patent}"
+refby = ReffedBy.new(patent)
 
-patentpage = Nokogiri::HTML(open(refquery))
-
-doc = patentpage.xpath("//body/table")
-rows = doc.xpath("//table//tr[1 <= position() and position() < 200]/td[2]/a/text()")
-#puts rows
-rows.each do |r|
-  puts "0" + r.to_s.gsub(/,/,'')
+describe ReffedBy do
+  it "works"
 end
