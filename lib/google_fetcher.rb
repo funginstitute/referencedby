24cdcb4b19bc39f9f111388b55396da6b0bf7640
Dave Doolin <david.doolin@gmail.com>
2013-01-11 15:43:19 -0800

    start of cleanup for google fetcher

--- lib/google_fetcher.rb
+++ lib/google_fetcher.rb
@@ -5,23 +5,13 @@ require 'nokogiri'
 require 'open-uri'
 require 'rspec'
 
-# this class is to fetch a patent from either Google or USPTO
-class PatentFetcher
-  
-  @patentpage
+class GoogleFetcher
 
   def initialize(argv)
-    # URL for search on patent number defined above
-    #patentquery = "http://www.google.com/patents/US#{argv}"
-	puts "Fetching patent #{argv}"
-    @patentpage = Nokogiri::HTML(open("http://www.google.com/patents/US#{argv}"))
+    patentpage = Nokogiri::HTML(open("http://www.google.com/patents/US#{argv}"))
     outFile = File.new("../data/google/#{argv}.html", "a+")
-    outFile.puts(@patentpage)
+    outFile.puts(patentpage)
     outFile.close
-  end
-
-  def patentpage
-    @patentpage
   end
 
 end
