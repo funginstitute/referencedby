016d27cdf976931a7a2abe274ad12f5fc4d7b8f0
Dave Doolin <david.doolin@gmail.com>
2013-01-11 15:35:31 -0800

    added lowest number

--- lib/patent_generator.rb
+++ lib/patent_generator.rb
@@ -2,10 +2,12 @@
 
 # must be Ruby version 1.9 +
 
+# Lowest patent number available is 03858241
+
 # method that returns n random numbers between [from, to]
 class PatentGenerator
 	@patentlist
-	def initialize(n=5,from=4000000, to=8000000) # by default, n = 5, from = 4000000, to = 8000000
+	def initialize(n=5,from=4000000, to=8000000)
 		to = to+1 # ensures that the range include 'to'
 		@patentlist = n.times.map{ from +Random.rand(to-from) }
 	end
